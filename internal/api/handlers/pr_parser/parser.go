af85ff4093e16bde3f8c163bea0cd63f81a9f87c
MB-Air Esneider <esneidergrvc@gmail.com>
2025-04-11 20:02:25 -0500

    try parsing approach

--- internal/api/handlers/pr_parser/parser.go
+++ internal/api/handlers/pr_parser/parser.go
@@ -24,21 +24,40 @@ func ParsePRTitle(prTitle string) (*PrTitle, error) {
 	re := regexp.MustCompile(pattern)
 
 	parts := strings.Split(prTitle, "|")
+
 	issueId := strings.ReplaceAll(parts[0], "(", "")
 	issueId = strings.ReplaceAll(issueId, ")", "")
+	issueId = strings.TrimSpace(issueId)
 
+	typeAndLayer := strings.Split(parts[1], ":")
+	typeAndLayerSplit := strings.Split(typeAndLayer[0], "[")
+	issueTypeValue := strings.TrimSpace(typeAndLayerSplit[0])
+
+	layer := typeAndLayerSplit[1]
+	layer = strings.ReplaceAll(layer, "]", "")
+	layer = strings.TrimSpace(layer)
+
+	title := typeAndLayer[1]
+	title = strings.TrimSpace(title)
+
+	version := strings.TrimSpace(parts[2])
+
+	fmt.Println("Extracted issueType:", issueTypeValue)
 	fmt.Println("Extracted Issue ID:", issueId)
+	fmt.Println("Extracted Layer:", layer)
+	fmt.Println("Extracted Title:", title)
+	fmt.Println("Extracted Version:", version)
 	matches := re.FindStringSubmatch(prTitle)
 	fmt.Println("Matches found:", matches, len(matches))
-	if len(matches) != 6 {
-		return nil, ErrInvalidFormat
-	}
+	// if len(matches) != 6 {
+	// 	return nil, ErrInvalidFormat
+	// }
 
 	return &PrTitle{
-		IssueId:   matches[1],
-		IssueType: matches[2],
-		AppLayer:  matches[3],
-		Title:     matches[4],
-		Version:   matches[5],
+		IssueId:   issueId,
+		IssueType: issueTypeValue,
+		AppLayer:  layer,
+		Title:     title,
+		Version:   version,
 	}, nil
 }
